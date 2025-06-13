1e3e79eb3295dc8eb851b940f7239049a4c9e615
Massimo <massimo.musso@edu-its.it>
2025-06-13 23:20:14 +0200

    animazione cards completa

--- js/main.js
+++ js/main.js
@@ -1,86 +1,21 @@
 // SHOW MENU
 
-const navMenu = document.getElementById("nav-menu"),
-  navToggle = document.getElementById("nav-toggle");
+const navMenu = document.getElementById('nav-menu'),
+  navToggle = document.getElementById('nav-toggle');
 
 if (navToggle) {
-  navToggle.addEventListener("click", () => {
-    navMenu.classList.toggle("show-menu");
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('show-menu');
   });
 }
 
-const navLinks = document.querySelectorAll(".nav__link");
+const navLinks = document.querySelectorAll('.nav__link');
 
 const linkAction = () => {
-  navMenu.classList.remove("show-menu");
+  navMenu.classList.remove('show-menu');
 };
 
-navLinks.forEach((link) => {
-  link.addEventListener("click", linkAction);
-});
-
-// CARDS ANIMATION
-
-// const cardsContainer = document.querySelector('.about__cards-container');
-// const cards = document.querySelectorAll('.card');
-
-// function rotateCards() {
-//   let angle = 0;
-//   cards.forEach((card, index) => {
-//     if (card.classList.contains('away')) {
-//       card.style.transform = `translateY(-120vh) rotate(-48deg)`;
-//     } else {
-//       card.style.transform = `rotate(${angle}deg)`;
-//       angle -= 10;
-
-//       card.style.zIndex = cards.length - index;
-//     }
-//   });
-// }
-
-// rotateCards();
-
-// window.addEventListener('scroll', () => {
-//   let distance = window.innerHeight * 0.5;
-
-//   let topVal = cardsContainer.getBoundingClientRect().top;
-
-//   let index = -1 * (topVal / distance + 1);
-
-//   index = Math.floor(index);
-
-//   for (let i = 0; i < cards.length; i++) {
-//     if (i <= index) {
-//       cards[i].classList.add('away');
-//     } else {
-//       cards[i].classList.remove('away');
-//     }
-//   }
-
-//   rotateCards();
-// });
-
-gsap.registerPlugin(ScrollTrigger);
-
-ScrollTrigger.batch(".about__card", {
-  onEnter: batch => {
-    gsap.to(batch, {
-      opacity: 1,
-      y: 0,
-      duration: 0.8,
-      ease: "power3.out",
-      stagger: 0.1,
-    });
-  },
-  onLeaveBack: batch => {
-    gsap.to(batch, {
-      opacity: 0,
-      y: 50,
-      duration: 0.6,
-      stagger: 0.05,
-    });
-  },
-  start: "top 80%",
-  toggleActions: "play none none reverse"
+navLinks.forEach(link => {
+  link.addEventListener('click', linkAction);
 });
 
