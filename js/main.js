24163c7212da7be82e266c826fbb6b2272d6e879
Massimo <massimo.musso@edu-its.it>
2025-06-14 12:13:40 +0200

    animazioni e ritocchi

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 // SHOW MENU
 
 const navMenu = document.getElementById('nav-menu'),
-  navToggle = document.getElementById('nav-toggle');
+  navToggle = document.getElementById('nav-toggle'),
+  nav = document.querySelector('.nav');
 
 if (navToggle) {
   navToggle.addEventListener('click', () => {
     navMenu.classList.toggle('show-menu');
+    nav.classList.toggle('nav__open');
   });
 }
 
@@ -19,3 +21,28 @@ navLinks.forEach(link => {
   link.addEventListener('click', linkAction);
 });
 
+// CHANGE HEADER BACKGROUND-COLOR
+
+const cards = document.querySelectorAll('.card');
+
+const observer = new IntersectionObserver(
+  entries => {
+    let delay = 0;
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        // Imposta un delay crescente per ogni card
+        entry.target.style.transitionDelay = `${delay}s`;
+        entry.target.classList.add('animate');
+        delay += 0.2; // aumenta il delay per la prossima card
+      } else {
+        entry.target.classList.remove('animate');
+        entry.target.style.transitionDelay = '0s';
+      }
+    });
+  },
+  {
+    threshold: 0.1,
+  }
+);
+
+cards.forEach(card => observer.observe(card));
