d035433bd31817e118aa93a639226f0cd5e57559
Olga Kutuzova <okutuzova@ukr.net>
2025-06-21 12:45:14 +0200

    Another version of reviews

--- js/main.js
+++ js/main.js
@@ -400,3 +400,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
   validateStep();
 });
+
+// Swiper
+const swiper = new Swiper('.js-testimonials-slider', {
+  grabCursor: true,
+  spaceBetween:30,
+  pagination: {
+    el: '.js-testimonials-pagination',
+    clickable: true
+  },
+  breakpoints: {
+    767:{
+      slidesPerView: 2
+    }
+  }
+
+});
